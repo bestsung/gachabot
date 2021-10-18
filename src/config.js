eafa7f8b07ac1f9964ba87321cb940e8f553e2ae
bestsung <bestsung@hotmail.com>
2021-10-19 02:39:04 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   owners: [],
   nodes: [{
-    identifier: 'NODE-MAIN-1',
-    host: 'lavalink-repl.bestsungnwtk.repl.co',
-    password: '_bestsung@2021',
-    port: 443,
-    secure: true,
+    identifier: 'NODE-MAIN-2',
+    host: 'lavalink.darrennathanael.com',
+    password: 'whatwasthelastingyousaid',
+    port: 2095,
+    secure: false,
   }],
-  prefix: 'g!',
+  prefix: '!',
   updateSlash: false,
 };
