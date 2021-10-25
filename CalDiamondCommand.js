3528e29ac7f94cd3f1b38e8fa4b87874466974a8
bestsung <bestsung@hotmail.com>
2021-10-25 23:37:46 +0700

    Add files via upload

---
+++ CalDiamondCommand.js
@@ -0,0 +1,55 @@
+const { Command, Argument } = require('discord-akairo');
+const { CreateEmbed } = require('../../Utility/CreateEmbed');
+
+module.exports = class CalCommand extends Command {
+  constructor() {
+    super('Cal', {
+      aliases: ['diamond','dm'],
+      description: {
+        content: 'Change music volume',
+      },
+      category: 'คำนวณ',
+      cooldown: 3000,
+      args: [
+        {
+          id: 'diamond',
+          type: Argument.range('number', 0, 100000001),
+          match: 'rest',
+          prompt: {
+            start: 'จำนวน __เพชร__ ที่คุณได้รับ?',
+            retry: '**⛔ |** คุณป้อนจำนวนไม่ถูกต้อง',
+          },
+        },
+
+        {
+          id: 'money',
+          type: Argument.range('number', 0, 100000001),
+          match: 'rest',
+          prompt: {
+            start: 'จำนวน __เงิน__ ที่คุณจ่าย?',
+            retry: '**⛔ |** คุณป้อนจำนวนไม่ถูกต้อง',
+          },
+        },
+		
+      ],
+    });
+  }
+
+  async exec(msg, { diamond,money }) {
+
+    //100฿=Diamond?
+    let diamond_1 = (diamond + money) * 100;
+
+    //difference
+    let difference = ((diamond_1 - 366.6) / 366.6)*100;
+	
+
+    try {
+      const GuildPlayers = this.client.erela.players.get(msg.guild.id);
+      return msg.channel.send({ embeds: [CreateEmbed('info', `**สรุป**\n[💎] \`${diamond}\`\n[💵] \`${money}\`฿\n\n**ผลลัพธ์**\n[💵] 100฿ = [💎] \`${diamond_1}\`\n\n**สรุป**\n✔️ ถูกกว่าเติมเอง \`${difference}\``)] });
+    } catch (e) {
+      this.client.logger.error(e.message);
+      return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | เกิดข้อผิดพลาด')] });
+    }
+  }
+};
